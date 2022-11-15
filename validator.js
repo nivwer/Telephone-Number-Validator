7dcdb35d5890bbdb5bbf215eae42d55f71b52d3e
nivwer <nivwerr@gmail.com>
2022-11-15 03:33:42 -0300

    update validator.js

--- validator.js
+++ validator.js
@@ -6,15 +6,19 @@ function telephoneCheck(str) {
 function validator(){
     let number = document.getElementById("number").value;
     let validate = document.getElementById("number");
+    let fa = document.getElementById("fa");
 
     number = telephoneCheck(number);
 
-    validate.classList.remove("validated", "invalidated")
+    validate.classList.remove("validated", "invalidated");
+    fa.classList.remove("fa-circle-check", "fa-circle-xmark");
 
     if (number == true){
         validate.classList.add("validated");
+        fa.classList.add("fa-circle-check");
     }
     else {
         validate.classList.add("invalidated");
+        fa.classList.add("fa-circle-xmark");
     }
 }
